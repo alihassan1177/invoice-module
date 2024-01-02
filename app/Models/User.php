f99ce112cd154d5d590ca0f5d3240785be218d51
alihassan1177 <ah927481@gmail.com>
2024-01-02 13:54:54 +0500

    added migrations for invoice and invoice categories

--- app/Models/User.php
+++ app/Models/User.php
@@ -18,9 +18,9 @@ class User extends Authenticatable
      * The attributes that are mass assignable.
      *
      * @var array
-     */
+     */ 
     protected $fillable = [
-        'name', 'email', 'password',
+        'name', 'email', 'password', 'address', 'account_no'
     ];
 
     /**
--- database/migrations/2024_01_02_083024_create_invoices_table.php
+++ database/migrations/2024_01_02_083024_create_invoices_table.php
@@ -15,6 +15,17 @@ return new class extends Migration
     {
         Schema::create('invoices', function (Blueprint $table) {
             $table->id();
+            $table->string("invoice_no");
+            $table->foreignId("invoice_category_id")->references("id")->on("invoice_categories");
+            $table->foreignId("user_id")->references("id")->on("users");
+            $table->text("products")->nullable();
+            $table->dateTime("issue_date");
+            $table->dateTime("due_date");
+            $table->integer("total_amount");
+            $table->integer("paid_amount");
+            $table->integer("tax_percentage");
+            $table->text("notes");
+            $table->string("status");
             $table->timestamps();
         });
     }
